2ac9c2761c6078120f0d51679e528468db0375b7
lucas <lucas.wijnen@outlook.com>
2025-05-21 12:37:45 +0200

    dikke toevoegingen voor de site

--- account.php
+++ account.php
@@ -10,5 +10,27 @@
 <?php
 include 'header.php';
 ?>
+<div class="login-center-wrapper">
+    <div class="login-container">
+        <h2>Log in of maak een account aan</h2>
+
+        <form class="login-form" method="post" action="verwerk_login.php">
+            <label for="email">E-mailadres</label>
+            <input type="email" id="email" name="email" placeholder="E-mailadres" required />
+
+            <div class="password-row">
+                <label for="password">Wachtwoord</label>
+                <a href="#">Wachtwoord vergeten?</a>
+            </div>
+            <input type="password" id="password" name="password" placeholder="Wachtwoord" required />
+
+            <button type="submit">Log in</button>
+
+            <div class="signup-text">
+                Heb je nog geen account? <a href="accountmaken.php">Meld je nu aan</a>
+            </div>
+        </form>
+    </div>
+</div>
 </body>
 </html>
--- contact.php
+++ contact.php
@@ -11,5 +11,36 @@
 include 'header.php';
 ?>
 
+
+<section class="spoed-contact">
+    <h2>Heeft je vraag spoed?</h2>
+    <p>Sta je al op het vliegveld? Of vertrekt je vlucht al over 2 uur?</p>
+    <p>Dan kun je ons het beste bellen.</p>
+
+    <label for="land-select" class="bold-label">Waar bel je vandaan?</label>
+    <select id="land-select">
+        <option>Nederland</option>
+    </select>
+
+    <div class="spoed-kaart">
+        <h3>Nederland</h3>
+        <a class="telefoon-link" >+31 (0) 12 34 56 789</a>
+        <p class="tarief">(lokaal tarief)</p>
+        <p>Ons Service Center helpt je graag van maandag t/m zondag van 8:00 tot 22.00 uur (Central European Time). Lokale tijden kunnen afwijken.</p>
+        <p>Voor telefonische boekingen rekenen wij €10 administratiekosten.</p>
+
+        <div class="talenbox">
+            <p><b>Beschikbare talen</b></p>
+            <div class="taalrij">
+                <span>Engels</span><span>05:00 - 23:00</span>
+            </div>
+            <div class="taalrij">
+                <span>Nederlands</span><span>08:00 - 22:00</span>
+            </div>
+        </div>
+    </div>
+</section>
+
+
 </body>
 </html>
